e8a27066c587fed5e77e71129d0becb64d7c0aed
Daniel <danielnatsufire@gmail.com>
2022-06-15 23:31:43 -0300

    POST /tweets

--- db.js
+++ db.js
@@ -1,1 +1,2 @@
 export const users = []
+export const tweets = []
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import cors from 'cors'
-import { users } from './db.js'
+import { users, tweets } from './db.js'
 
 const app = express()
 const PORT = 5000
 app.use(cors())
 app.use(express.json())
+let userId = 1
+let tweetId = 1
 
-app.post('/sign-up', async (req, res) => {
-    const user = {...req.body}
+app.post('/sign-up', (req, res) => {
+    users.push({ userId, ...req.body })
+    userId++
 
-    users.push(user)
+    res.send('OK')
+})
+
+app.post('/tweets', (req, res) => {
+    const userAvatar = users.find(user => user.username === req.body.username)
+    tweets.push({ tweetId, ...req.body, avatar: userAvatar.avatar })
+    tweetId++
+    
     res.send('OK')
 })
 
