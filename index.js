b086859961eb4bb9f7b15a75cd8d1c235f6f79f0
Daniel <danielnatsufire@gmail.com>
2022-06-16 04:54:42 -0300

    Bonus - POST /tweets recebendo username por Header

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ import { isEmpty, validateObj, verifyURL } from './validations.js'
 
 const app = express()
 const PORT = 5000
+
 app.use(cors())
 app.use(express.json())
+
 let userId = 1
 let tweetId = 1
 
@@ -18,7 +20,7 @@ app.post('/sign-up', (req, res) => {
             res.status(422).send('Coloque uma URL de imagem válida!')
         }
 
-        users.push({ userId, ...req.body })
+        users.push({ id: userId, ...req.body })
         userId++
     }
 
@@ -26,22 +28,31 @@ app.post('/sign-up', (req, res) => {
 })
 
 app.post('/tweets', (req, res) => {
+    const userAvatar = users.find(user => user.username === req.headers.user)
+
     if(isEmpty({...req.body}) || !validateObj({...req.body}, 'tweet')) {
-
-        res.status(400).send('Todos os campos são obrigatórios!')
-    } 
-
-    const userAvatar = users.find(user => user.username === req.body.username)
-    tweets.push({ tweetId, ...req.body, avatar: userAvatar.avatar })
-    tweetId++
+        res.status(400).send('Todos os campos são obrigatórios!') 
+    } else {
+        if(userAvatar === undefined) {
+            res.status(401).send('Faltando o headers do nome do usuário!')
+        }
     
-    res.status(201).send('OK')
+        tweets.push({
+            id: tweetId, 
+            username: req.headers.user, 
+            ...req.body, 
+            avatar: userAvatar.avatar 
+        })
+        tweetId++
+        
+        res.status(201).send('OK')
+    }
 })
 
 app.get('/tweets', (req, res) => {
     if(isEmpty({...req.body})) res.sendStatus(400)
 
-    const tenTweets = tweets.sort((a, b) => b.tweetId - a.tweetId).filter((_, i) => i < 10)
+    const tenTweets = tweets.sort((a, b) => b.id - a.id).filter((_, i) => i < 10)
 
     res.send(tenTweets)
 })
